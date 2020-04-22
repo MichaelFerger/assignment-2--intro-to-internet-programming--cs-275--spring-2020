aab7b75efb01609a714c5f9b127121ff93535dfd
MichaelFerger <60191614+MichaelFerger@users.noreply.github.com>
2020-04-22 18:05:30 -0400

    Add const for gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,5 @@
 const { src, dest, series, watch } = require(`gulp`);
+const htmlCompressor = require(`gulp-htmlmin`);
+const htmlValidator = require(`gulp-html`);
+const jsCompressor = require(`gulp-uglify`);
+const babel = require(`gulp-babel`);
