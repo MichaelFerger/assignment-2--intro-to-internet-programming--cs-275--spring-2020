0572587ddf938ce190196b4c940fe47fc24be35c
MichaelFerger <60191614+MichaelFerger@users.noreply.github.com>
2020-04-22 18:05:55 -0400

    add compressHTML code

--- gulpfile.js
+++ gulpfile.js
@@ -3,3 +3,8 @@ const htmlCompressor = require(`gulp-htmlmin`);
 const htmlValidator = require(`gulp-html`);
 const jsCompressor = require(`gulp-uglify`);
 const babel = require(`gulp-babel`);
+let compressHTML = () => {
+    return src([`dev/html/*.html`,`dev/html/**/*.html`])
+        .pipe(htmlCompressor({collapseWhitespace: true}))
+        .pipe(dest(`prod`));
+};
