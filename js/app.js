86806105b4c6c8c48f12215fe64bf7006e2f32b7
MichaelFerger <60191614+MichaelFerger@users.noreply.github.com>
2020-04-24 18:04:14 -0400

    Add function to display cali rice div on button click

--- js/app.js
+++ js/app.js
@@ -3,3 +3,9 @@ function showDiv1() {
     document.getElementById(`whiteRice`).style.display = `block`;
     document.getElementById(`caliRice`).style.display = `none`;
 }
+
+function showDiv2() {
+    document.getElementById(`caliRice`).style.display = `block`;
+    document.getElementById(`whiteRice`).style.display = `none`;
+}
+/* eslint-enable */
