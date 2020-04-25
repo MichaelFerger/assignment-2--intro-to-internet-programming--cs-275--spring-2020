6ae8a583b8b35da9b55d53c60368ef80486690ab
MichaelFerger <60191614+MichaelFerger@users.noreply.github.com>
2020-04-24 20:55:59 -0400

    fix id names for white rice

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-/* eslint-disable */
 function showDiv1() {
     document.getElementById(`whiteRice`).style.display = `block`;
     document.getElementById(`caliRice`).style.display = `none`;
@@ -8,16 +7,13 @@ function showDiv2() {
     document.getElementById(`caliRice`).style.display = `block`;
     document.getElementById(`whiteRice`).style.display = `none`;
 }
-/* eslint-enable */
+
 function convertWhite() {
-    var amount = document.getElementById(`whiteSubmit`).value;
-    var water;
+    let amount = document.getElementById(`whiteSubmit`).value;
+    let water;
     water = amount * 2;
-    // alert(amount);
-    // document.write(`<p>Combine ${amount} cup of rice with ${water} cups
-    //      of water and 1 Tbsp olive oil. Bring to a boil, then reduce heat to the
-    //      lowest setting. Cook for about 18 minutes.</p>`);
     document.getElementById(`whiteRecipe`).style.display = `block`;
-    document.getElementById(`amount`).innerHTML = amount;
-    document.getElementById(`water`).innerHTML = water;
+    document.getElementById(`amount1`).innerHTML = amount;
+    document.getElementById(`water1`).innerHTML = water;
+}
 }
