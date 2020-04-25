3989ba55c439ae11ee0a29078e6646b70964e9b4
MichaelFerger <60191614+MichaelFerger@users.noreply.github.com>
2020-04-24 20:31:23 -0400

    Add function to convert white rice recipe

--- js/app.js
+++ js/app.js
@@ -9,3 +9,15 @@ function showDiv2() {
     document.getElementById(`whiteRice`).style.display = `none`;
 }
 /* eslint-enable */
+function convertWhite() {
+    var amount = document.getElementById(`whiteSubmit`).value;
+    var water;
+    water = amount * 2;
+    // alert(amount);
+    // document.write(`<p>Combine ${amount} cup of rice with ${water} cups
+    //      of water and 1 Tbsp olive oil. Bring to a boil, then reduce heat to the
+    //      lowest setting. Cook for about 18 minutes.</p>`);
+    document.getElementById(`whiteRecipe`).style.display = `block`;
+    document.getElementById(`amount`).innerHTML = amount;
+    document.getElementById(`water`).innerHTML = water;
+}
